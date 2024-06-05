321ee9620dbe73c5344967ec7a1cc5cbce1a60b8
Anshul Chaturvedi <129001479+AnshulChaturvedi@users.noreply.github.com>
2024-06-06 04:36:16 +0530

    Time: 11 ms (98.71%), Space: 94.4 MB (90.21%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -3,33 +3,45 @@ public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size();
         int m = nums2.size();
-        vector<int> mergedArray;
-
+        int mergeSize = n + m;
+        int index2 = mergeSize/2;
+        int index1 = index2 - 1;
+        int cnt = 0;
+        int ele1 = INT_MIN;
+        int ele2 = INT_MIN;   
         int i=0,j=0;
+
         while(i<n && j<m){
             if(nums1[i] < nums2[j]){
-                mergedArray.push_back(nums1[i]);
+                if(cnt == index1) ele1 = nums1[i];
+                if(cnt == index2) ele2 = nums1[i];
+                cnt++;
                 i++;
             }
             else{
-                mergedArray.push_back(nums2[j]);
+                if(cnt == index1) ele1 = nums2[j];
+                if(cnt == index2) ele2 = nums2[j];
+                cnt++;
                 j++;
             }
         }
         while(i<n){
-            mergedArray.push_back(nums1[i]);
-            i++;
+           if(cnt == index1) ele1 = nums1[i];
+           if(cnt == index2) ele2 = nums1[i];
+           cnt++;
+           i++;
         }
         while(j<m){
-            mergedArray.push_back(nums2[j]);
+            if(cnt == index1) ele1 = nums2[j];
+            if(cnt == index2) ele2 = nums2[j];
+            cnt++;
             j++;
         }
 
-        int size = mergedArray.size();
-        if(size&1){
-            return mergedArray[size/2];
+        if(mergeSize&1){
+            return ele2;
         }
-        return (mergedArray[size/2] + mergedArray[size/2 - 1])/2.0;
+        return (double)(ele1 + ele2)/2.0;
 
     }
 };
