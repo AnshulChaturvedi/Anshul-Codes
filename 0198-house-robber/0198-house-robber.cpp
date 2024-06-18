36685d8ddfd43017d5d2d35e548e7f1727e4dae9
Anshul Chaturvedi <129001479+AnshulChaturvedi@users.noreply.github.com>
2024-06-18 19:10:29 +0530

    Time: 0 ms (100%), Space: 9 MB (81.7%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -4,13 +4,15 @@ public:
         int n = nums.size();
         if(n == 1) return nums[0];
 
-        vector<int> dp(n+1,-1);
-        // dp[i] reprents the maxi amount of money robbed till ith index
-        dp[0] = nums[0];
-        dp[1] = max(nums[0],nums[1]);
+        int a = nums[0];
+        int b = max(nums[0],nums[1]);
+        int c = max(a,b);
         for(int i=2;i<n;i++){
-            dp[i] = max(dp[i-1],dp[i-2] + nums[i]);
+            c = max(a + nums[i],b);
+            int temp = b;
+            b = c;
+            a = temp;
         }
-        return dp[n-1];
+        return c;
     }
 };
