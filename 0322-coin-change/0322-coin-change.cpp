432be58fc16475387270dac594be54651f46903b
Anshul Chaturvedi <129001479+AnshulChaturvedi@users.noreply.github.com>
2024-06-21 00:52:04 +0530

    Time: 37 ms (50.71%), Space: 51 MB (13.09%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+
+        vector<vector<int>> dp(n+1,vector<int> (amount+1,1000000));
+        //dp[i][j] represents the mini no. of coins req to make amount j with i items
+
+        for(int i=1;i<=n;i++){
+            dp[i][0] = 0;
+        }
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=amount;j++){
+                if(coins[i-1] <= j ){
+                    dp[i][j] = min(dp[i-1][j],dp[i][j-coins[i-1]]+1);
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return (dp[n][amount] == 1000000) ? -1 : dp[n][amount];
+    }
+};
