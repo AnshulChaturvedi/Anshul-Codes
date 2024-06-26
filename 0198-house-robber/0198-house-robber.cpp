7cc2b65ed8be7856cc02e4fc335ff071bb97d07e
Anshul Chaturvedi <129001479+AnshulChaturvedi@users.noreply.github.com>
2024-06-26 16:03:26 +0530

    Time: 0 ms (100%), Space: 9 MB (81.83%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -3,15 +3,15 @@ public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         if(n == 1) return nums[0];
+        if(n == 2) return max(nums[0],nums[1]);
 
         int a = nums[0];
         int b = max(nums[0],nums[1]);
-        int c = max(a,b);
+        int c;
         for(int i=2;i<n;i++){
             c = max(a + nums[i],b);
-            int temp = b;
+            a = b;
             b = c;
-            a = temp;
         }
         return c;
     }
