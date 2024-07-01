003d4c0f9d28e9c851120448b36536f19b942837
Anshul Chaturvedi <129001479+AnshulChaturvedi@users.noreply.github.com>
2024-07-01 17:45:20 +0530

    Time: 10 ms (18.71%), Space: 15.8 MB (83.85%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,57 +1,45 @@
 class Solution {
 public:
-    vector<int> delr{1,0,-1,0};
-    vector<int> delc{0,-1,0,1};
-    int ans = 0;
-    
     int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>> vis(n,vector<int> (m,0));
-        
+        int freshCnt = 0;
         queue<pair<int,int>> q;
-        // to store the indexes of rotten orange
-        
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j] == 2){//rotten orange
+                if(grid[i][j] == 2){
                     q.push({i,j});
-                    vis[i][j] = 2;
                 }
-                //fresh orange
-                else if(grid[i][j] == 1){
-                    vis[i][j] = 1;
-                }
+                else if(grid[i][j] == 1) freshCnt++;
             }
         }
-        
+        if(freshCnt == 0) return 0;
+
+        int ans = 0;
+        int delr[4] = {0,+1,0,-1};
+        int delc[4] = {+1,0,-1,0};
+
         while(!q.empty()){
             int size = q.size();
-            ans++;
-            
-            for(int j=0;j<size;j++){
+            bool isAnyFresh = false;
+
+            for(int i=0;i<size;i++){
                 int r = q.front().first;
                 int c = q.front().second;
                 q.pop();
-                
-                for(int i=0;i<4;i++){
-                    int nrow = r + delr[i];
-                    int ncol = c + delc[i];
-                    if( nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol] == 1 &&  vis[nrow][ncol] == 1){
+                for(int j=0;j<4;j++){
+                    int nrow = r + delr[j];
+                    int ncol = c + delc[j];
+                    if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol] == 1){
+                        grid[nrow][ncol] = 2;
+                        freshCnt--;
+                        isAnyFresh = true;
                         q.push({nrow,ncol});
-                        vis[nrow][ncol] = 2;
                     }
                 }
             }
+            if(isAnyFresh == true) ans++;
         }
-         for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(vis[i][j] == 1){
-                    return -1;
-                }
-            }
-        }
-        
-        return max(0,ans-1);//to remove the final initial state
+        return (freshCnt == 0) ? ans : -1;
     }
 };
