ccb13c01036dcdd70c8166c9246aee75156d7b0b
Anshul Chaturvedi <129001479+AnshulChaturvedi@users.noreply.github.com>
2024-07-07 16:39:06 +0530

    Time: 0 ms (100%), Space: 8.4 MB (59.28%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    int memo[101][101][201];
-    bool solve(int i,int j,int k,string &s1,string &s2,string &s3,int &n1,int &n2,int &n3){
-        if(k>=n3) return true;
-        if(memo[i][j][k] != -1) return memo[i][j][k];
-        if(i<n1 && j<n2 && s1[i] == s3[k] && s2[j] == s3[k]){
-            return memo[i][j][k] = solve(i+1,j,k+1,s1,s2,s3,n1,n2,n3) || solve(i,j+1,k+1,s1,s2,s3,n1,n2,n3);
+    int memo[101][101];
+    bool solve(int i,int j,string &s1,string &s2,string &s3,int &n1,int &n2,int &n3){
+        if(i+j>=n3) return true;
+        if(memo[i][j] != -1) return memo[i][j];
+        if(i<n1 && j<n2 && s1[i] == s3[i+j] && s2[j] == s3[i+j]){
+            return memo[i][j] = solve(i+1,j,s1,s2,s3,n1,n2,n3) || solve(i,j+1,s1,s2,s3,n1,n2,n3);
         }
-        else if(i<n1 && s1[i] == s3[k]){
-            return memo[i][j][k] = solve(i+1,j,k+1,s1,s2,s3,n1,n2,n3);
+        else if(i<n1 && s1[i] == s3[i+j]){
+            return memo[i][j] = solve(i+1,j,s1,s2,s3,n1,n2,n3);
         }
-        else if(j<n2 && s2[j] == s3[k]){
-            return memo[i][j][k] = solve(i,j+1,k+1,s1,s2,s3,n1,n2,n3);
+        else if(j<n2 && s2[j] == s3[i+j]){
+            return memo[i][j] = solve(i,j+1,s1,s2,s3,n1,n2,n3);
         }
-        return memo[i][j][k] = false;
+        return memo[i][j] = false;
     }
 
     bool isInterleave(string s1, string s2, string s3) {
         int n1 = s1.length(), n2=s2.length(), n3=s3.length();
         memset(memo,-1,sizeof(memo));
         if(n1 + n2 != n3) return false;
-        return solve(0,0,0,s1,s2,s3,n1,n2,n3);
+        return solve(0,0,s1,s2,s3,n1,n2,n3);
     }
 };
