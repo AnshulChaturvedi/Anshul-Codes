3f932140fb1d08c21213cb6778756e18d16ebde8
Anshul Chaturvedi <129001479+AnshulChaturvedi@users.noreply.github.com>
2024-07-26 17:48:33 +0530

    Time: 84 ms (19.82%), Space: 19.9 MB (16%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -3,32 +3,13 @@ public:
     int jump(vector<int>& nums) {
         int n = nums.size();
         if(n == 1) return 0;
-
-        vector<int> vis(n,0);
-        queue<int> q;
-        q.push(0);
-        vis[0] = 1;
-
-        int jumps = 0;
-
-        while(!q.empty()){
-            int size = q.size();
-            jumps++;
-            for(int i=0;i<size;i++){
-                int node = q.front();
-                q.pop();
-                for(int j=1;j<=nums[node];j++){
-                    int next = node + j;
-                    if(next >= n-1){
-                        return jumps;
-                    }
-                    if(!vis[next]){
-                        q.push(next);
-                        vis[next] = 1;
-                    }
-                }
+        vector<int> dp(n,INT_MAX);
+        dp[0] = 0;
+        for(int i=0;i<n;i++){
+            for(int j=1;j<=nums[i] && i+j<n;j++){
+                dp[i+j] = min(dp[i+j],dp[i] + 1);
             }
         }
-        return -1;
+        return dp[n-1];
     }
 };
