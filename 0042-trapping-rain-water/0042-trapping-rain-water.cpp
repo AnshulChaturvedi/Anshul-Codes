3435ea4da68e2dfacacf503e721daaa5bee5faeb
Anshul Chaturvedi <129001479+AnshulChaturvedi@users.noreply.github.com>
2024-08-11 16:15:33 +0530

    Time: 18 ms (11.36%), Space: 24.8 MB (6.89%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -2,23 +2,27 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size();
-        //for each building maxi water it can store is 
-        // min{leftMax, rightMax} - height of that building
-        vector<int> prefixMax(n);
-        prefixMax[0] = height[0];
-        for(int i=1;i<n;i++){
-            prefixMax[i] = max(prefixMax[i-1],height[i]);
-        }
-        vector<int> suffixMax(n);
-        suffixMax[n-1] = height[n-1];
-        for(int i=n-2;i>=0;i--){
-            suffixMax[i] = max(suffixMax[i+1],height[i]);
-        }
-        //
-        int total = 0;
-        for(int i=0;i<n;i++){
-            int leftMax = prefixMax[i], rightMax = suffixMax[i];
-            total += min(leftMax,rightMax)-height[i];
+        int total=0,leftMax=0,rightMax=0;
+        int l = 0, r = n-1;
+        while(l<r){
+            if(height[l] <= height[r]){
+                if(leftMax > height[l]){
+                    total += leftMax-height[l];
+                }
+                else{
+                    leftMax = height[l];
+                }
+                l++;
+            }
+            else{
+                if(rightMax > height[r]){
+                    total += rightMax-height[r];
+                }
+                else{
+                    rightMax = height[r];
+                }
+                r--;
+            }
         }
         return total;
     }
