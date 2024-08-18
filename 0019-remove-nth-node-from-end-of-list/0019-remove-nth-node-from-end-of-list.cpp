9ec24b8f4dd5bf6632eb9b4d03e53e9230ff7585
Anshul Chaturvedi <129001479+AnshulChaturvedi@users.noreply.github.com>
2024-08-18 10:34:37 +0530

    Time: 0 ms (100%), Space: 14.8 MB (23.17%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -14,21 +14,19 @@ public:
         if(head->next == NULL && n==1){
             return NULL;
         }
-        int len = 0;
-        ListNode *temp = head;
-        while(temp != NULL){
-            len++;
-            temp = temp->next;
+        ListNode *fast = head;
+        while(n--){
+            fast = fast->next;
         }
-        int pos = len-n;
-        if(pos == 0){
-            return head->next;
+        if(fast == NULL) return head->next;
+        ListNode *slow = head;
+        while(fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next;
         }
-        ListNode *curr = head;
-        for(int i=0;i<pos-1;i++){
-            curr = curr->next;
-        }
-        if(curr->next != NULL) curr->next = curr->next->next;
+        ListNode *nodetodelete = slow->next;
+        if(slow->next != NULL) slow->next = slow->next->next;
+        delete nodetodelete;
         return head;
     }
 };
