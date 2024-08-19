e969b5ab3f4ae9194251286de0c0a65229131aaf
Anshul Chaturvedi <129001479+AnshulChaturvedi@users.noreply.github.com>
2024-08-19 11:36:24 +0530

    Time: 3 ms (61.8%), Space: 7.5 MB (47.65%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,42 +1,14 @@
-// struct ListNode{
-//     int val;
-//     ListNode* next;
-//     ListNode(): val(0), next(nullptr){}
-//     ListNode(int x): val(x), next(nullptr){}
-//     ListNode(int x, ListNode* next): val(x), next(next){}
-// };
-
-ListNode* createList(int n){
-    ListNode* head = new ListNode(1);
-    ListNode* curr = head;
-    for(int i=2;i<=n;i++){
-        ListNode* nextNode = new ListNode(i);
-        curr->next = nextNode;
-        curr = nextNode;
-    }
-    curr->next = head;
-    return head;
-}
-
 class Solution {
 public:
-    int findTheWinner(int n, int k) {
-        if(k == 1) return n;
+    int findWinner(int n,int k){
+        if(n == 1) return 0;
+        int idx = findWinner(n-1,k);
+        idx = (idx + k) % n;
+        return idx;
+    }
 
-        ListNode* head = createList(n);
-        ListNode* curr = head;
-        ListNode* prev = NULL;
-        while(curr->next != curr){
-            for(int i=1;i<k;i++){
-                prev = curr;
-                curr = curr->next;
-            }
-            prev->next = curr->next;
-            delete curr;
-            curr = prev->next;
-        }
-        int ans = curr->val;
-        delete(curr);
-        return ans;
+    int findTheWinner(int n, int k) {
+        int res_index = findWinner(n,k);
+        return res_index + 1;
     }
 };
