58967c0e53819c15bb0f210a500da85dc8aeb50a
Anshul Chaturvedi <129001479+AnshulChaturvedi@users.noreply.github.com>
2024-08-25 15:59:30 +0530

    Time: 15 ms (51.12%), Space: 8.3 MB (45.51%) - LeetHub

--- 1278-palindrome-partitioning-iii/1278-palindrome-partitioning-iii.cpp
+++ 1278-palindrome-partitioning-iii/1278-palindrome-partitioning-iii.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int dp[101][101];
+    
     int makePalindrome(string &s,int st,int e){
         int cnt = 0;
         while(st<e){
@@ -9,29 +10,24 @@ public:
         return cnt;
     }
 
-    int solve(int i,int n,string &s,int cuts){
-        if(i == n) return (cuts == 0) ? 0 : 1e6;
-        if(cuts == 0){
-            return makePalindrome(s,i,n-1);
-        }
+    int solve(int i,int n,string &s,int k){
+        if(i == n) return (k == 0) ? 0 : 1e6;
+        if(k == 0) return 1e6;
 
-        if(dp[i][cuts] != -1) return dp[i][cuts];
+        if(dp[i][k] != -1) return dp[i][k];
 
         int ans = 1e6;
 
-        for(int k=i;k<n;k++){
-            int charChange = makePalindrome(s,i,k) + solve(k+1,n,s,cuts-1);
+        for(int j=i;j<n;j++){
+            int charChange = makePalindrome(s,i,j) + solve(j+1,n,s,k-1);
             ans = min(ans,charChange);
         }
-        return dp[i][cuts] = ans;
+        return dp[i][k] = ans;
     }
 
     int palindromePartition(string s, int k) {
-        //
-        if(s == "aea" || s == "abab") return 1;
-        //
         int n = s.length();
         memset(dp,-1,sizeof(dp));
-        return solve(0,n,s,k-1);
+        return solve(0,n,s,k);
     }
 };
