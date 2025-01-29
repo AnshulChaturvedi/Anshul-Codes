137f73b379b21275d69d4cf41427218780edbbca
Anshul Chaturvedi <129001479+AnshulChaturvedi@users.noreply.github.com>
2025-01-29 23:41:44 +0530

    Time: 0 ms (100%), Space: 13.1 MB (47.29%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
     vector<int> parent;
+    vector<int> size;
     int find_set(int v){
         if(v == parent[v])
             return v;
-        return find_set(parent[v]);
+        return parent[v] = find_set(parent[v]);
     }
     void union_sets(int a, int b){
         a = find_set(a);
         b = find_set(b);
-        if(a != b)
+        if(a != b){
+            if(size[a] < size[b])
+                swap(a,b);
             parent[b] = a;
+            size[a] += size[b];
+        }
     }
     
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int n = edges.size();
         parent.resize(n+1);
-
+        size.resize(n+1);
         for(int i=1; i<=n; i++){
             parent[i] = i;
+            size[i] = 1;
         }
 
         for(auto edge : edges){
