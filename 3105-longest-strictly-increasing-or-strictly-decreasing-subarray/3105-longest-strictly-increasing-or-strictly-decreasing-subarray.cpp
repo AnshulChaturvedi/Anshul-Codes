a620becf6c3bec7592ecae9afcd8fe97250b9c5d
Anshul Chaturvedi <129001479+AnshulChaturvedi@users.noreply.github.com>
2025-02-03 12:35:13 +0530

    Time: 0 ms (100%), Space: 28 MB (7.06%) - LeetHub

--- 3105-longest-strictly-increasing-or-strictly-decreasing-subarray/3105-longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
+++ 3105-longest-strictly-increasing-or-strictly-decreasing-subarray/3105-longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     int longestMonotonicSubarray(vector<int>& nums) {
-        int ansForLie = 1, cnt1 = 1, lie = nums[0];
-        int ansForLde = 1, cnt2 = 1, lde = nums[0];
+        int ansForLie = 1, cnt1 = 1;
+        int ansForLde = 1, cnt2 = 1;
         for(int i=1; i<nums.size(); i++){
-            if(nums[i] > lie){
+            if(nums[i] > nums[i-1]){
                 cnt1++;
-                ansForLie = max(ansForLie, cnt1);
-            }
-            else{
-                cnt1 = 1;
-            }
-            if(nums[i] < lde){
-                cnt2++;
-                ansForLde = max(ansForLde, cnt2);
-            }
-            else{
                 cnt2 = 1;
             }
-            lie = lde = nums[i];
+            else if(nums[i] < nums[i-1]){
+                cnt2++;
+                cnt1 = 1;
+            }
+            else{
+                cnt1 = cnt2 = 1;
+            }
+            ansForLie = max(ansForLie, cnt1);
+            ansForLde = max(ansForLde, cnt2);
         }
         return max(ansForLie, ansForLde);
     }
