f1bf9b11022008a4b36059a6d409dbb57c411d21
Anshul Chaturvedi <129001479+AnshulChaturvedi@users.noreply.github.com>
2025-02-05 23:48:59 +0530

    Time: 0 ms (100%), Space: 8.3 MB (61.87%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
     bool areAlmostEqual(string s1, string s2) {
-        if(s1 == s2) return true;
         int cnt = 0;
-        vector<int> freq1(26,0);
-        vector<int> freq2(26,0);
+        int firstDiffIdx = 0, secondDiffIdx = 0;
         for(int i=0; i<s1.length(); i++){
-            if(s1[i] != s2[i]) cnt++;
-            if(cnt > 2) return false;
-            freq1[s1[i]-'a']++;
-            freq2[s2[i]-'a']++;
-        }
-        if(cnt == 1) return false;
-        if(cnt == 2){
-            for(int i=0; i<26; i++){
-                if(freq1[i] != freq2[i]) return false;
+            if(s1[i] != s2[i]){
+                cnt++;
+                if(cnt > 2) return false;
+                else if(cnt == 1) firstDiffIdx = i;
+                else secondDiffIdx = i;
             }
         }
-        return true;
+        return s1[firstDiffIdx] == s2[secondDiffIdx] && 
+                s1[secondDiffIdx] == s2[firstDiffIdx];
     }
 };
