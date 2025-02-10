37287fb500366e367394ad4c76c6b3c1f1ac1865
Anshul Chaturvedi <129001479+AnshulChaturvedi@users.noreply.github.com>
2025-02-10 16:51:08 +0530

    Time: 0 ms (100%), Space: 8.3 MB (87.14%) - LeetHub

--- 3174-clear-digits/3174-clear-digits.cpp
+++ 3174-clear-digits/3174-clear-digits.cpp
@@ -2,16 +2,10 @@ class Solution {
 public:
     string clearDigits(string s) {
         string ans;
-        stack<char> st;
         for(auto ch : s){
-            if(ch >= '0' && ch <= '9') st.pop();
-            else st.push(ch);
+            if(ch >= 'a' && ch <= 'z') ans += ch;
+            else ans.pop_back();
         }
-        while(!st.empty()){
-            ans += st.top();
-            st.pop();
-        }
-        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
