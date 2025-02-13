e62e6e7b3015e165b972811047af8f6b1ed582dd
Anshul Chaturvedi <129001479+AnshulChaturvedi@users.noreply.github.com>
2025-02-14 00:44:34 +0530

    Time: 102 ms (34.22%), Space: 91.5 MB (74%) - LeetHub

--- 3066-minimum-operations-to-exceed-threshold-value-ii/3066-minimum-operations-to-exceed-threshold-value-ii.cpp
+++ 3066-minimum-operations-to-exceed-threshold-value-ii/3066-minimum-operations-to-exceed-threshold-value-ii.cpp
@@ -1,36 +1,51 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
-        map<long long,long long> mpp;
-        for(auto num : nums) mpp[num]++;
+        // map<long long,long long> mpp;
+        // for(auto num : nums) mpp[num]++;
+        // int cnt = 0;
+        // while(!mpp.empty() && mpp.begin()->first < k){
+        //     auto it1 = mpp.begin();
+        //     long long num1 = it1->first;
+        //     if(it1->second >= 2){
+        //         it1->second -= 2;
+        //         if(it1->second == 0){
+        //             mpp.erase(it1);
+        //         }
+        //         long long val = 3 * num1;
+        //         mpp[val]++;
+        //     }
+        //     else{
+        //         it1->second--;
+        //         if(it1->second == 0){
+        //             mpp.erase(it1);
+        //         }
+        //         auto it2 = next(it1);
+        //         long long num2 = it2->first;
+        //         it2->second--;
+        //         if(it2->second == 0){
+        //             mpp.erase(it2);
+        //         }
+        //         long long mini = min(num1, num2);
+        //         long long maxi = max(num1, num2);
+        //         long long val = mini * 2 + maxi;
+        //         mpp[val]++;
+        //     }
+        //     cnt++;
+        // }
+        // return cnt;
+
+        
+        priority_queue<long long,vector<long long>,greater<long long>> pq(nums.begin(), nums.end());
+        //min-heap --> min elements at the top
         int cnt = 0;
-        while(!mpp.empty() && mpp.begin()->first < k){
-            auto it1 = mpp.begin();
-            long long num1 = it1->first;
-            if(it1->second >= 2){
-                it1->second -= 2;
-                if(it1->second == 0){
-                    mpp.erase(it1);
-                }
-                long long newVal = 3 * num1;
-                mpp[newVal]++;
-            }
-            else{
-                auto it2 = next(it1);
-                long long num2 = it2->first;
-                it1->second--;
-                if(it1->second == 0){
-                    mpp.erase(it1);
-                }
-                it2->second--;
-                if(it2->second == 0){
-                    mpp.erase(it2);
-                }
-                long long mini = min(num1, num2);
-                long long maxi = max(num1, num2);
-                long long val = mini * 2 + maxi;
-                mpp[val]++;
-            }
+        while(pq.size() >= 2 && pq.top() < k){
+            long long num1 = pq.top();
+            pq.pop();
+            long long num2 = pq.top();
+            pq.pop();
+            long long val = min(num1, num2) * 2 + max(num1, num2);
+            pq.push(val);
             cnt++;
         }
         return cnt;
