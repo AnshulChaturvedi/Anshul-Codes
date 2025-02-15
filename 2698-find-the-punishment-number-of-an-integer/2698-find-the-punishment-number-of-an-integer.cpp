27a816eb964e9b6036c55e9bbb7afeca14ccd477
Anshul Chaturvedi <129001479+AnshulChaturvedi@users.noreply.github.com>
2025-02-15 15:24:35 +0530

    Time: 19 ms (76.32%), Space: 7.8 MB (99.5%) - LeetHub

--- 2698-find-the-punishment-number-of-an-integer/2698-find-the-punishment-number-of-an-integer.cpp
+++ 2698-find-the-punishment-number-of-an-integer/2698-find-the-punishment-number-of-an-integer.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
-    bool check(int i, int currSum, string s, int num){
-        if(i >= s.length()) return currSum == num;
-        if(currSum > num) return false;
-        bool poss = false;
-        for(int j=i; j<s.length(); j++){
-            string sub = s.substr(i, j-i+1);
-            int val = stoi(sub);
-            poss = poss || check(j+1, currSum+val, s, num);
-            if(poss) return true;
-        }
-        return poss;
+    bool check(int sq, int currSum, int num){
+        if(sq == 0) return currSum == num;
+        return check(sq/10, currSum + sq%10, num) || 
+                check(sq/100, currSum + sq%100, num) ||
+                    check(sq/1000, currSum + sq%1000, num) ||
+                        check(sq/10000, currSum + sq%10000, num);
     }
 
     int punishmentNumber(int n) {
         int res = 0;
-        for(int i=1; i<=n; i++){
+        for(int i=1; i<=n; i++){///  O(n)*X
             int sq = i*i;
-            string s = to_string(sq);
-            if(check(0, 0, s, i)){
+            if(check(sq, 0, i)){
                 res += sq;
             }
         }
-        return res;
+        return res; // T.C = O(n * 2^(log10(n^2)));
     }
 };
