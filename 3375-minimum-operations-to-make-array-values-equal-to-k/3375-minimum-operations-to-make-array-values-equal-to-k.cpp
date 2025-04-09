6f7377a79e32e8699bdb37e0389986e4b27806c0
Anshul Chaturvedi <129001479+AnshulChaturvedi@users.noreply.github.com>
2025-04-10 00:50:48 +0530

    Time: 14 ms (37.11%), Space: 32.3 MB (67.3%) - LeetHub

--- 3375-minimum-operations-to-make-array-values-equal-to-k/3375-minimum-operations-to-make-array-values-equal-to-k.cpp
+++ 3375-minimum-operations-to-make-array-values-equal-to-k/3375-minimum-operations-to-make-array-values-equal-to-k.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
-        //cnt num of dist elements greater than K
-        // auto it = find(nums.begin(), nums.end(), k);
-        // if(it == nums.end()) return -1;
-
         unordered_set<int> st;
-        int lesser = 0;
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i] > k) st.insert(nums[i]);
-            if(nums[i] < k) lesser++;
+        for(auto num : nums){
+            if(num < k) return -1;
+            if(num > k) st.insert(num);
         }
-        if(lesser == 0 && st.size() == 0) return 0;
-        if(lesser > 0) return -1;
+        if(st.size() == 0) return 0;
         return st.size();
     }
 };
