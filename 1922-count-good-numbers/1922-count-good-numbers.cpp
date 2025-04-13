4a57d9517b627d61656c063cde2e186c37ad3b1d
Anshul Chaturvedi <129001479+AnshulChaturvedi@users.noreply.github.com>
2025-04-13 17:39:13 +0530

    Time: 0 ms (100%), Space: 7.8 MB (96.45%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.cpp
+++ 1922-count-good-numbers/1922-count-good-numbers.cpp
@@ -1,31 +1,22 @@
-using ll = long long;
-
 class Solution {
-private:
-    ll power(ll a, ll b){
-        ll result = 1;
-        while(b>0){
-            if(b&1){
-                result = ((result%M)*(a%M))%M;
-            }
-            a = ((a%M)*(a%M))%M;
-            b /= 2;
-        }
-        return result;
-    }
 public:
-    const int M = 1e9 + 7;
+    const int MOD = 1000000007;
+
+    long long modExp(long long base, long long exp){
+        long long res = 1;
+        while(exp > 0){
+            if(exp & 1){
+                res = (res * base) % MOD;
+            }
+            base = (base * base) % MOD;
+            exp /= 2;
+        }
+        return res;
+    }
+
     int countGoodNumbers(long long n) {
-        // for even place 5 choices (0,2,4,6,8)
-        // for odd places 4 chices (2,3,5,7)
-        ll oddIndexes = n/2;
-        ll evenIndexes = oddIndexes;
-        if(n&1) evenIndexes++;
-
-        ll ans1 = power(5,evenIndexes);
-        ll ans2 = power(4,oddIndexes);
-
-        return ((ans1%M)*(ans2%M))%M;
-
+        long long evenIdx = (n + 1) / 2; //ceil
+        long long oddIdx = n / 2; //floor
+        return (modExp(5, evenIdx) * modExp(4, oddIdx))% MOD;
     }
 };
