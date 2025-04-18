c8d6d881a999eb6d945acd0cdf4f09e44624e47d
Anshul Chaturvedi <129001479+AnshulChaturvedi@users.noreply.github.com>
2025-04-19 00:03:55 +0530

    Time: 3 ms (83.88%), Space: 10 MB (61.25%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-    string RLE(string &s, int n){
-        if(n == 1) return s;
-        int m = s.length();
-        string str = "";
-        int i=0;
-        while(i<m){
-            int same = 0, j = i;
-            while(s[j] == s[i] && j<m){
-                j++;
-                same++;
-            }
-            str += to_string(same) + s[i];
-            i = j;
-        }
-        return RLE(str, n-1);
-    }
-
     string countAndSay(int n) {
-        string s = "1";
-        if(n == 1) return s;
-        return RLE(s, n);
+        if(n == 1) return "1";
+        string temp = "1";
+        for(int i=2; i<=n; i++){
+            string curr = "";
+            int cnt = 1;
+            for(int j=1; j<=temp.size(); j++){
+                if(j<temp.size() && temp[j] == temp[j-1]){
+                    cnt++;
+                }
+                else{
+                    curr += to_string(cnt);
+                    curr += temp[j-1];
+                    cnt = 1;
+                }
+            }
+            temp = curr;
+        }
+        return temp;
     }
 };
