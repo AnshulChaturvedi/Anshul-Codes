c1b097fab05817060dbbb46107d0871d2bb28f8d
Anshul Chaturvedi <129001479+AnshulChaturvedi@users.noreply.github.com>
2025-04-22 14:32:11 +0530

    Time: 3 ms (40.26%), Space: 8.3 MB (96.35%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -2,20 +2,14 @@ class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
         string s;
-        int i=0;
-        while(i < word1.size() && i < word2.size()){
-            s += word1[i];
-            s += word2[i];
+        int i=0, n1 = word1.size(), n2 = word2.size();
+        while(i < n1 && i < n2){
+            s.push_back(word1[i]);
+            s.push_back(word2[i]);
             i++;
         }
-        while(i < word1.size()){
-            s += word1[i];
-            i++;
-        }
-        while(i < word2.size()){
-            s += word2[i];
-            i++;
-        }
+        if(i < n1) s.append(word1.data() + i, n1 - i);
+        if(i < n2) s.append(word2.data() + i, n2 - i);
         return s;
     }
 };
