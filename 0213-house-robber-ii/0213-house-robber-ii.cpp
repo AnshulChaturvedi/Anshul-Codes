8383d28186081432318187879b14c83f7078d03d
Anshul Chaturvedi <129001479+AnshulChaturvedi@users.noreply.github.com>
2025-09-11 17:08:39 +0530

    Time: 4 ms (1.61%), Space: 9.4 MB (100%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,23 +1,25 @@
 class Solution {
-public:
-    int dp[101];
-    int solve(vector<int>& nums, int s, int e){
-        if(s > e) return 0;
-        if(dp[s] != -1) return dp[s];
-        int take = solve(nums, s+2, e) + nums[s];
-        int notTake = solve(nums, s+1, e);
-        return dp[s] = max(take, notTake);
+private:
+    int robRange(vector<int> &nums,int s,int e){
+        int n = e - s + 1;//no. of houses range who can be robbed
+        if(n == 1){
+            return nums[s];
+        }
+        vector<int> dp(n,-1);
+        //dp[i] represents the maxi money robbed till ith index house
+        dp[0] = nums[s];
+        dp[1] = max(nums[s],nums[s+1]);
+        for(int i=2;i<n;i++){
+            dp[i] = max(dp[i-1],dp[i-2]+nums[i+s]);
+        }
+        return dp[n-1];
     }
 
+public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         if(n == 1) return nums[0];
-        if(n == 2) return max(nums[0], nums[1]);
-        if(n == 3) return max({nums[0], nums[1], nums[2]});
-        memset(dp,-1,sizeof(dp));
-        int first = solve(nums,0,n-2);
-        memset(dp,-1,sizeof(dp));
-        int second = solve(nums,1,n-1);
-        return max(first, second);
+
+        return max(robRange(nums,0,n-2),robRange(nums,1,n-1));
     }
 };
