bf5386e375e66e87e95a4292ad8821311e30601c
Anshul Chaturvedi <129001479+AnshulChaturvedi@users.noreply.github.com>
2025-09-11 16:59:00 +0530

    Time: 0 ms (100%), Space: 10.3 MB (67.43%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,26 +1,23 @@
 class Solution {
-private:
-    int solve(int s, int e, vector<int> &nums,vector<int> &dp){
-        if(s>e){
-            return 0;
-        }
-        if(dp[s] != -1){
-            return dp[s];
-        }
-        int rob = solve(s+2,e,nums,dp) + nums[s];
-        int notRob = solve(s+1,e,nums,dp);
-        return dp[s] = max(rob,notRob);
-    }
 public:
+    int dp[101];
+    int solve(vector<int>& nums, int s, int e){
+        if(s > e) return 0;
+        if(dp[s] != -1) return dp[s];
+        int take = solve(nums, s+2, e) + nums[s];
+        int notTake = solve(nums, s+1, e);
+        return dp[s] = max(take, notTake);
+    }
+
     int rob(vector<int>& nums) {
         int n = nums.size();
         if(n == 1) return nums[0];
-        if(n == 2) return max(nums[0],nums[1]);
-        if(n == 3) return max(nums[0],max(nums[1],nums[2]));
-
-        vector<int> dp1(n,-1);
-        vector<int> dp2(n,-1);
-
-        return max(solve(0,n-2,nums,dp1),solve(1,n-1,nums,dp2));
+        if(n == 2) return max(nums[0], nums[1]);
+        if(n == 3) return max({nums[0], nums[1], nums[2]});
+        memset(dp,-1,sizeof(dp));
+        int first = solve(nums,0,n-2);
+        memset(dp,-1,sizeof(dp));
+        int second = solve(nums,1,n-1);
+        return max(first, second);
     }
 };
