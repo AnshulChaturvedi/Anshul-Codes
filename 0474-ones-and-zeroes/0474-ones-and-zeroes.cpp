d47e27ccacf2169eb8a2dd575c06706a81210f4e
Anshul Chaturvedi <129001479+AnshulChaturvedi@users.noreply.github.com>
2025-09-11 17:25:50 +0530

    Time: 162 ms (46.75%), Space: 39.1 MB (60.43%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
@@ -1,27 +1,21 @@
 class Solution {
-private:
-    int memo[1001][101][101];
-    int solve(int i,int m,int n,vector<string> &strs){
-        if(i >= strs.size() || m<0 || n<0){
-            return 0;
-        }
-        if(memo[i][m][n] != -1){
-            return memo[i][m][n];
-        }
-
-        int noOfZeros = count(strs[i].begin(),strs[i].end(),'0');
-        int noOfOnes = count(strs[i].begin(),strs[i].end(),'1');
-
-        int pick = 0;
-        if(noOfZeros <= m && noOfOnes <= n){
-            pick = solve(i+1,m-noOfZeros,n-noOfOnes,strs) + 1;
-        }
-        int notPick = solve(i+1,m,n,strs);
-        return memo[i][m][n] = max(pick,notPick);
-    }
 public:
+    int dp[601][101][101];
+    int solve(int i, int m, int n, vector<string>& strs){
+        if(i >= strs.size()) return 0;
+        if(dp[i][m][n] != -1) return dp[i][m][n];
+        int oneCnt = count(strs[i].begin(),strs[i].end(),'1');
+        int zeroCnt = count(strs[i].begin(),strs[i].end(),'0');
+        int take = 0;
+        if(oneCnt <= n && zeroCnt <= m){
+            take = solve(i+1,m-zeroCnt,n-oneCnt,strs) + 1;
+        }
+        int skip = solve(i+1,m,n,strs);
+        return dp[i][m][n] = max(take,skip);
+    }
+
     int findMaxForm(vector<string>& strs, int m, int n) {
-        memset(memo,-1,sizeof(memo));
+        memset(dp,-1,sizeof(dp));
         return solve(0,m,n,strs);
     }
 };
